22f4bb98c1a2e0346d003ce2f056a2de90b7466c
kromp <-@>
2024-01-04 06:33:24 +0300

    add after close drivers and chromedriver 120 version

--- src/main/java/com/github/simileyskiy/hooks/Hook.java
+++ src/main/java/com/github/simileyskiy/hooks/Hook.java
@@ -2,8 +2,11 @@ package com.github.simileyskiy.hooks;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Screenshots;
+import com.codeborne.selenide.WebDriverRunner;
 import io.cucumber.java.*;
 import io.qameta.allure.Allure;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -17,6 +20,7 @@ public class Hook {
     public void setUp() {
         Configuration.browserSize = "1920x1080";
         System.setProperty("chromeoptions.args", "--remote-allow-origins=*");
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver-120.exe");
 //        Configuration.headless = true;
     }
 
@@ -28,6 +32,18 @@ public class Hook {
             } catch (FileNotFoundException fileNotFoundException) {
                 System.out.println("Fail add screenShot: " + fileNotFoundException.getMessage());
             }
+        }
+    }
+
+
+    @After
+    public static void tearDown() {
+        try {
+            WebDriver driver = WebDriverRunner.getWebDriver();
+            driver.close();
+            driver.quit();
+        } catch (WebDriverException e) {
+            e.printStackTrace();
         }
     }
 }
